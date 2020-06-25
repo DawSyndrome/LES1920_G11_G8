08fbcebb72886f9a98fe848dfc2651f416f8b1d6
DawSyndrome <56260881+DawSyndrome@users.noreply.github.com>
2020-06-25 04:47:54 +0100

    Update urls.py

--- LES/gottem/urls.py
+++ LES/gottem/urls.py
@@ -6,21 +6,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('register', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
 
     
-    path('change_password', views.changpw, name='changpw'),
+    path('change_password/', views.changpw, name='changpw'),
 
-    path('reset_password', views.resetpw, name='resetpw'),
+    path('reset_password/', views.resetpw, name='resetpw'),
     path('reset_password/<int:uid>/<str:token>', views.resetpw, name='resetpw_return'),
 
 
     path("", views.home, name='home'),
 
 
-    path('lista', views.index, name='index'),
+    path('lista/', views.index, name='index'),
     path('lista/<int:page>', views.index, name='index_paged'),
 
     path('apagar/<int:id>', views.delete_user, name='delete_user'),
